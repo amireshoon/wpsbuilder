ee14f40a6c660883281c4c166952e0aab145fc44
unknown <abtin.yas53@gmail.com>
2020-11-11 16:20:40 +0330

    Added basic methods

--- wpsBuilder.php
+++ wpsBuilder.php
@@ -2,6 +2,51 @@
 
 if (!class_exists('wpsBuilder')) {
     class wpsBuilder {
-    
+        
+        public function __construct() {
+            
+        }
+
+        public function setPageTitle($pageTitle) {
+
+        }
+
+        public function attachToMenu($parentSlug) {
+
+        }
+
+        public function addMenu($slug) {
+
+        }
+
+        public function setMenuTitle($menuTitle) {
+
+        }
+
+        /**
+         * The capability required for this menu to be displayed to the user.
+         * 
+         * @refrence https://wordpress.org/support/article/roles-and-capabilities/
+         */
+        public function capability($capability) {
+
+        }
+
+        /**
+         * The icon uses to render menu page. Only available when using @addMenu.
+         * 
+         * @refrence https://developer.wordpress.org/resource/dashicons
+         */
+        public function iconUrl($iconUrl) {
+
+        }
+
+        public function position($position) {
+
+        }
+
+        public function build() {
+            
+        }
     }
 }
